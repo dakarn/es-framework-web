5749ca2fcb4f58bbb43e21ede9983ae2f3c511f6
myusername <myusername@myemaildomain.com>
2018-09-30 20:19:21 +0400

    ffcfc

--- bootstrap-web.php
+++ bootstrap-web.php
@@ -4,6 +4,7 @@ define('PATH_LOADER', __DIR__ . '/vendor/autoload.php');
 define('PATH_SYSTEM', __DIR__ . '/vendor/es-framework-kernel/system/');
 define('PATH_APP', __DIR__  . '/app/');
 define('PSR_4', true);
+define('IS_DEV', \is_file('dev.bootstrap.php'));
 
 include_once PATH_SYSTEM . 'Helper/Util.php';
 
@@ -15,8 +16,14 @@ $event = $event->installEvents(new \System\EventListener\EventManager());
 $appKernel = new \App\AppKernel();
 $appKernel->installMiddlewares()->installProviders();
 
+$env = 'PROD';
+
+if (IS_DEV) {
+	$env = 'DEV';
+}
+
 $application = (new \System\Kernel\TypesApp\WebApp())
-	->setEnvironment('PROD')
+	->setEnvironment($env)
 	->setAppEvent($event)
 	->setAppKernel($appKernel)
 	->setApplicationType('Web');
